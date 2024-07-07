d156972991d50ab300ae6cbfdfbdf09d9d539b73
Danny Roma <daniel.roma@gmail.com>
2024-07-07 07:01:01 -0700

    duck token

--- duck.sh
+++ duck.sh
@@ -1,1 +1,1 @@
-echo url="https://www.duckdns.org/update?domains=droma82&token=bb5dfd90-a62a-4a23-a27e-1e3c00f49f93&ip=" | curl -k -o ~/Documents/docker/duck.log -K -
+echo url="https://www.duckdns.org/update?domains=droma82&token=$(<.duck_token)&ip=" | curl -k -o ~/docker/duck.log -K -
